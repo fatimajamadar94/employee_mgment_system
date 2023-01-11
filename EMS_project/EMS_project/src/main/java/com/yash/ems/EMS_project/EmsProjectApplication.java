c0cd8fe35e847e2260d2a1608482adea5e4775d1
fatimajamadar94 <114378443+fatimajamadar94@users.noreply.github.com>
2023-01-11 12:30:05 +0530

    Add files via upload
    emps first update

---
+++ EMS_project/EMS_project/src/main/java/com/yash/ems/EMS_project/EmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yash.ems.EMS_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsProjectApplication.class, args);
+	}
+
+}
