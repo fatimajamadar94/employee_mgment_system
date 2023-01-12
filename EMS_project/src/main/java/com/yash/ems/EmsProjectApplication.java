c057c87f0573d0af025e38a89cd099600614d44b
Fatima Zakir Jamadar <fatima.jamadar@Yash.com>
2023-01-12 14:27:01 +0530

    new

--- EMS_project/src/main/java/com/yash/ems/EmsProjectApplication.java
+++ EMS_project/src/main/java/com/yash/ems/EmsProjectApplication.java
@@ -7,19 +7,12 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-@EnableSwagger2
-@ConfigurationProperties
+
 @SpringBootApplication
 public class EmsProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsProjectApplication.class, args);
-	}
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.yash.ems"))
-				.build();
 	}
 
 }
--- EMS_project/src/main/java/com/yash/ems/configuration/swaggerConfig.java
+++ EMS_project/src/main/java/com/yash/ems/configuration/swaggerConfig.java
@@ -11,21 +11,19 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration
-//@EnableSwagger2WebMvc
-//@EnableWebMvc
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 public class swaggerConfig {
 
 	
-//	    @Bean
-//	    public Docket api() { 
-//	        return new Docket(DocumentationType.SWAGGER_2)  
-//	          .select()                                  
-//	          .apis(RequestHandlerSelectors.basePackage("com.yash.ems"))              
-//	          .paths(PathSelectors.any())                          
-//	          .build();                                           
-//	    }
+	    @Bean
+	    public Docket api() { 
+	        return new Docket(DocumentationType.SWAGGER_2)  
+	          .select()                                  
+	          .apis(RequestHandlerSelectors.basePackage("com.yash.ems"))              
+	          .paths(PathSelectors.any())                          
+	          .build();                                           
+    }
 //	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //	     registry.addResourceHandler("swagger-ui.html")
 //	       .addResourceLocations("classpath:/META-INF/resources/");
